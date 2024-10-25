414774e000d038be9e1dfba60b96bb2dabc49061
Johnson Towoju <johnsontodimu@gmail.com>
2024-10-25 11:45:59 +0100

    chore(config): update name and desc

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,8 +4,8 @@ import vue from '@vitejs/plugin-vue';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
     manifest: {
-        name: 'YouTube Simple View (Hide Sidebar and more)',
-        description: 'Customize your YouTube experience with settings and features to enhance your focus.',
+        name: 'YouTube Simple View - Hide distractions & more',
+        description: 'Focus tool for YouTube: control distracting elements and autoplay settings to stay focused.',
         version: '0.9.2',
         permissions: ['webNavigation', 'storage', 'scripting'],
         host_permissions: ['*://*.youtube.com/*'],
