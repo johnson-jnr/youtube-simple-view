69c2d5fa5ff5108a5ed05d15b718afa5ebf64b78
Johnson Towoju <johnsontodimu@gmail.com>
2024-10-25 10:36:08 +0100

    chore: update version, rm from package.json

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     manifest: {
         name: 'YouTube Simple View (Hide Sidebar and more)',
         description: 'Customize your YouTube experience with settings and features to enhance your focus.',
-        version: '0.9.1',
+        version: '0.9.2',
         permissions: ['webNavigation', 'storage', 'scripting'],
         host_permissions: ['*://*.youtube.com/*'],
     },
