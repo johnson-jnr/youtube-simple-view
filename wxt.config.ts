b4965a2c5807a4f74e7cec67db42e1efe9573e71
Johnson Towoju <johnsontodimu@gmail.com>
2024-10-25 12:02:37 +0100

    chore(config): update name to support firefox text limit

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
     manifest: {
-        name: 'YouTube Simple View - Hide distractions & more',
+        name: 'YouTube Simple View: Hide distractions & more',
         description: 'Focus tool for YouTube: control distracting elements and autoplay settings to stay focused.',
         version: '0.9.2',
         permissions: ['webNavigation', 'storage', 'scripting'],
